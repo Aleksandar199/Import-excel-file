f14366e1ba27e41fbac35ffd8e721dfb483fe2f5
Aleksandar <79797076+Aleksandar199@users.noreply.github.com>
2022-11-07 08:29:07 +0100

    Delete footer.php

--- inc/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bg-dark text-center fixed-bottom">
-      <!-- Copyright -->
-      <div class="text-center p-1 text-white">
-       Aptiv Serbia - Copyright &copy; <?php echo date('Y');?>. <!--, all rights reserved --> 
-      <!-- Copyright -->
-    </footer>
-  </body>
-</html>
