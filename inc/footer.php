8b0304bef39a475ba5f522469f655866cf451476
Aleksandar <79797076+Aleksandar199@users.noreply.github.com>
2022-11-07 08:28:10 +0100

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-dark text-center fixed-bottom">
+      <!-- Copyright -->
+      <div class="text-center p-1 text-white">
+       Aptiv Serbia - Copyright &copy; <?php echo date('Y');?>. <!--, all rights reserved --> 
+      <!-- Copyright -->
+    </footer>
+  </body>
+</html>
